4859f1d20749b21c52e67801a08248bfbc287481
HKUST\xlibb <sylvia.lixh@gmail.com>
2017-04-10 21:10:16 +0800

    Train Effective Hamiltonian with IO management

--- GetEffective.py
+++ GetEffective.py
@@ -3,7 +3,8 @@ __author__ = 'xlibb'
 import numpy as np
 import wolff
 import originalH
-
+import pickle
+import matplotlib.pyplot as plt
 
 class geteffectiveH:
     """
@@ -24,8 +25,8 @@ class geteffectiveH:
         :return Jn:
         """
         energy,correlations=originalH.energyVsCorrelation(temperature,K,self.Nx,self.Ny,mcsetN)
-        Jlist=np.linalg.lstsq(correlations,energy)[0]
-        return Jlist
+        Jlist,erro,gamma=np.linalg.lstsq(correlations,energy)
+        return Jlist,erro,gamma
 
 
     def iterativeWolff(self,temperaturelist,K,mcsetN):
@@ -34,14 +35,29 @@ class geteffectiveH:
         :return:
         """
         hightemperature=temperaturelist[0]
-        Jlist=self.fit_originalHsample(hightemperature,K,mcsetN)
-        for temperature in temperaturelist[1:]:
+
+        Jlist,erro,gamma=self.fit_originalHsample(hightemperature,K,mcsetN)
+        Jdata=np.zeros((len(temperaturelist),3))
+        errodata=np.zeros(len(temperaturelist))
+        Jdata[0]=Jlist
+        errodata[0]=erro
+
+        for (idx,temperature) in enumerate(temperaturelist[1:]):
             spinsample_chain=wolff.spinsample_chain(temperature,Jlist[0],self.Nx,self.Ny,mcsetN)
             energy,correlations=originalH.energyVsCorrelation(temperature,K,self.Nx,self.Ny,mcsetN,spinsample_chain)
-            Jlist=np.linalg.lstsq(correlations,energy)[0]
+            Jlist,erro,gamma=np.linalg.lstsq(correlations,energy)
+            Jdata[idx+1]=Jlist
+            errodata[idx+1]=erro
 
+        with open("data/Jdata.txt","wb") as f:
+            pickle.dump(Jdata,f)
+        with open("data/errodata.txt","wb") as f:
+            pickle.dump(errodata,f)
         return Jlist
 
+def loaddata(filename):
+    with open("data/"+filename,"rb") as f:
+        return pickle.load(f)
 
 def train(K,mcsetN,Nx,Ny):
     """
@@ -53,13 +69,22 @@ def train(K,mcsetN,Nx,Ny):
     :param Ny: size on the y direction
     :return: final Jlist
     """
-    temperaturelist=np.linspace(5,2.49,50)
+    temperaturelist=np.linspace(5,2.49,10)
     trainer=geteffectiveH(Nx,Ny)
     Jlist=trainer.iterativeWolff(temperaturelist,K,mcsetN)
     print(Jlist)
 
+def get_trainingdata():
+    Jlist=loaddata("Jdata.txt")
+    error=loaddata("errodata.txt")
+    plt.figure()
+    plt.plot(Jlist[:,0])
+    plt.plot(Jlist[:,1]*20)
+    plt.figure()
+    plt.plot(error)
 
-train(0.2,mcsetN=10,Nx=10,Ny=10)
+
+train(0.2,mcsetN=50,Nx=10,Ny=100)
 
 
 
