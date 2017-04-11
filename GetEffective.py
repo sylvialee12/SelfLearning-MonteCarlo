193aac84a5c23bdcf618458ee7f70776c85c80a5
HKUST\xlibb <sylvia.lixh@gmail.com>
2017-04-11 12:54:32 +0800

    Linear Regression only on nearest site

--- GetEffective.py
+++ GetEffective.py
@@ -6,6 +6,7 @@ import originalH
 import pickle
 import matplotlib.pyplot as plt
 
+
 class geteffectiveH:
     """
     This is the class for the original Hamiltonian getting effective Hamiltonian
@@ -18,16 +19,14 @@ class geteffectiveH:
         self.Nx=Nx
         self.Ny=Ny
 
-
     def fit_originalHsample(self,temperature,K,mcsetN):
         """
         Fitting n parameters using the linear regression
         :return Jn:
         """
         energy,correlations=originalH.energyVsCorrelation(temperature,K,self.Nx,self.Ny,mcsetN)
-        Jlist,erro,gamma=np.linalg.lstsq(correlations,energy)
-        return Jlist,erro,gamma
-
+        Jlist,erro,_,_=np.linalg.lstsq(correlations,energy)
+        return Jlist,erro
 
     def iterativeWolff(self,temperaturelist,K,mcsetN):
         """
@@ -36,22 +35,23 @@ class geteffectiveH:
         """
         hightemperature=temperaturelist[0]
 
-        Jlist,erro,gamma=self.fit_originalHsample(hightemperature,K,mcsetN)
+        Jlist,erro=self.fit_originalHsample(hightemperature,K,mcsetN)
         Jdata=np.zeros((len(temperaturelist),3))
         errodata=np.zeros(len(temperaturelist))
         Jdata[0]=Jlist
         errodata[0]=erro
 
         for (idx,temperature) in enumerate(temperaturelist[1:]):
-            spinsample_chain=wolff.spinsample_chain(temperature,Jlist[0],self.Nx,self.Ny,mcsetN)
+            spinsample_chain=wolff.spinsample_chain(temperature,abs(Jlist[0]),self.Nx,self.Ny,mcsetN)
             energy,correlations=originalH.energyVsCorrelation(temperature,K,self.Nx,self.Ny,mcsetN,spinsample_chain)
-            Jlist,erro,gamma=np.linalg.lstsq(correlations,energy)
+            # energy,correlations=wolff.energyVsCorrelation(temperature,Jlist[0],self.Nx,self.Ny,mcsetN)
+            Jlist,erro,_,_=np.linalg.lstsq(correlations,energy)
             Jdata[idx+1]=Jlist
             errodata[idx+1]=erro
 
-        with open("data/Jdata.txt","wb") as f:
+        with open("data/Jdata2.txt","wb") as f:
             pickle.dump(Jdata,f)
-        with open("data/errodata.txt","wb") as f:
+        with open("data/errodata2.txt","wb") as f:
             pickle.dump(errodata,f)
         return Jlist
 
@@ -69,23 +69,27 @@ def train(K,mcsetN,Nx,Ny):
     :param Ny: size on the y direction
     :return: final Jlist
     """
-    temperaturelist=np.linspace(5,2.49,10)
+    temperaturelist=np.linspace(10,2.49,40)
     trainer=geteffectiveH(Nx,Ny)
     Jlist=trainer.iterativeWolff(temperaturelist,K,mcsetN)
     print(Jlist)
 
 def get_trainingdata():
-    Jlist=loaddata("Jdata.txt")
-    error=loaddata("errodata.txt")
+    Jlist=loaddata("Jdata2.txt")
+    error=loaddata("errodata2.txt")
+    print(Jlist[:,0])
     plt.figure()
     plt.plot(Jlist[:,0])
     plt.plot(Jlist[:,1]*20)
+    plt.show()
     plt.figure()
     plt.plot(error)
+    plt.show()
 
 
-train(0.2,mcsetN=50,Nx=10,Ny=100)
 
+# train(0.2,mcsetN=100,Nx=10,Ny=10)
+get_trainingdata()
 
 
 
